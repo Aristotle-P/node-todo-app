f3ad03c288e68fa2a90f4511d4f9a128ae4d9569
Sully <aristotlepoultney@gmail.com>
2019-08-26 19:12:45 -0700

    updated dependencies for security

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_DB_URI;
 
-mongoose.connect(process.env.MONGO_DB_URI, {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useCreateIndex: true
   // useFindAndModify: false
